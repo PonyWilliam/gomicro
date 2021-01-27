d01bc88e4929d5a1dcbbf9015b9291f63164ec8c
william <tjw@dadiqq.cn>
2021-01-28 04:16:54 +0800

    run on consul

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/jinzhu/gorm v1.9.16
 	github.com/micro/go-micro/v2 v2.9.1
+	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1
+	github.com/micro/go-plugins/registry/consul/v2 v2.9.1
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
 	google.golang.org/protobuf v1.25.0
 )
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/util/log"
+	"github.com/micro/go-plugins/registry/consul/v2"
+	"strconv"
+	"time"
+	"user/common"
 	"user/domain/repository"
 	service2 "user/domain/service"
 	"user/handler"
@@ -11,21 +16,42 @@ import (
 )
 
 func main() {
+	//配置中心
+	consulConfig,err := common.GetConsualConfig("127.0.0.1",8500,"/micro/config")
+	if err != nil{
+		//记录日志
+		log.Error(err)
+	}
+	//注册中心
+	consulRegistry := consul.NewRegistry(func(options *registry.Options){
+		options.Addrs = []string{"127.0.0.1"}
+		options.Timeout = 10 * time.Second
+	})
+	//NEW service
 	srv := micro.NewService(
 		micro.Name("service.user"),
 		micro.Version("latest"),
+		//这里设置地址和需要暴露的端口
+		micro.Address("127.0.0.1:8081"),
+		//添加consul 作为注册中心
+		micro.Registry(consulRegistry),
 		)
-	srv.Init()
+	//路径中不用代前缀
+	mysqlInfo := common.GetMysqlFromConsul(consulConfig,"mysql")
 	//创建数据库
 	db,err := gorm.Open("mysql",
-		"gostudy:gostudy@tcp(39.107.65.116)/gostudy?charset=utf8&parseTime=True&loc=Local",
+		mysqlInfo.User+":"+mysqlInfo.Pwd+"@tcp("+mysqlInfo.Host + ":"+ strconv.FormatInt(mysqlInfo.Port,10) +")/"+mysqlInfo.DataBase+"?charset=utf8&parseTime=True&loc=Local",
 		)
 	if err!=nil{
-		fmt.Println(err)
+		log.Error(err)
 	}
 	defer db.Close()
 	//执行完所有后关闭数据库
 	db.SingularTable(true)
+
+	srv.Init()
+
+
 
 	rp:=repository.NewUserRepository(db)
 	rp.I()
@@ -37,9 +63,9 @@ func main() {
 	})
 	//绑定实例
 	if err!=nil{
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	if err:=srv.Run();err!=nil{
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 }
